b566e7a5e5fd36123e79310a123514ed9f4bac44
lauragift21 <laurandidi21@gmail.com>
2020-01-05 04:33:06 +0100

    upload dataset to datahub

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # data-wrangling-challenge
+
+Datahub Link: https://datahub.io/lauragift21/natural-gas-prices-dataset
---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const axios = require("axios")
+const cheerio = require('cheerio');
+const ObjectsToCsv = require('objects-to-csv');
+const url = "https://www.eia.gov/dnav/ng/hist/rngwhhdD.htm"
+
+axios(url).then(response => {
+  const html = response.data
+  const $ = cheerio.load(html);
+  const tableData = $('table:nth-of-type(3)');
+  let data = [];
+
+  tableData.each(function () {
+    const date = $(this).find('.B6').text();
+    const price = $(this).find('.B3').text();
+    data.push({
+      date,
+      price
+    })
+  })
+
+  async function printCsv(data) {
+    await new ObjectsToCsv(data).toDisk('./data.csv')
+    console.log(
+      await new ObjectsToCsv(data).toString()
+    );
+  }
+  printCsv(data);
+}).catch(console.error)
+
